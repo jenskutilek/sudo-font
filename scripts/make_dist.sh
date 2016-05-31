dc042d20c233335b8bab12e94679243f93dcb0d5
Jens Kutilek <webmail@kutilek.de>
2016-05-31 19:53:28 +0200

    Fix dist script

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -1,4 +1,7 @@
+#!/bin/sh
+
 rm ../dist/sudo.zip
-zip -r ../dist/sudo.zip ../sudo/ --exclude \*.DS_Store
+cd ..
+zip -r dist/sudo.zip sudo/ --exclude \*.DS_Store
 mv ~/Sites/kuti/download/sudo.zip ~/Sites/kuti/download/sudo_old.zip
-cp ../dist/sudo.zip ~/Sites/kuti/download/
+cp dist/sudo.zip ~/Sites/kuti/download/
