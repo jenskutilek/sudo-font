4ad2e366d2f4e84af2acf79ac0d83283da722f81
Jens Kutilek <webmail@kutilek.de>
2019-05-17 17:35:13 +0200

    Old dist script is not needed anymore

--- scripts/make_dist.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-rm ../dist/sudo.zip
-cd ..
-zip -r dist/sudo.zip sudo/ --exclude \*.DS_Store
-mv ~/Sites/kuti/download/sudo.zip ~/Sites/kuti/download/sudo_old.zip
-cp dist/sudo.zip ~/Sites/kuti/download/
