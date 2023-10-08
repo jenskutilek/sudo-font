ee062600e9c2ab17e946e4873013fd64b873870d
Jens Kutilek <webmail@kutilek.de>
2023-10-09 00:03:45 +0200

    Fix file names

--- scripts/prepare-deb.sh
+++ scripts/prepare-deb.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-version=`git describe --tags`
+version=`git describe --tags | awk 'match($0, /v([0-9]+\.[0-9]+)/) {print substr($0, RSTART+1, RLENGTH-1)}'`
 rm -rf dist/deb
 mkdir -p dist/deb/sudo-font
 git archive --format=tar.gz ${version} --prefix=sudo-font-${version}/ > dist/deb/sudo-font/sudo-font-${version}.tar.gz
