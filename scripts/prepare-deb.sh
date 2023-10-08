474b2a80413e3e9ecd48f093433afd3564fa7573
Jens Kutilek <webmail@kutilek.de>
2023-10-09 00:05:59 +0200

    git archive needs the v tag

--- scripts/prepare-deb.sh
+++ scripts/prepare-deb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 version=`git describe --tags | awk 'match($0, /v([0-9]+\.[0-9]+)/) {print substr($0, RSTART+1, RLENGTH-1)}'`
-rm -rf dist/deb
+rm -rf dist/deb/sudo-font/*
 mkdir -p dist/deb/sudo-font
-git archive --format=tar.gz ${version} --prefix=sudo-font-${version}/ > dist/deb/sudo-font/sudo-font-${version}.tar.gz
+git archive --format=tar.gz v${version} --prefix=sudo-font-${version}/ > dist/deb/sudo-font/sudo-font-${version}.tar.gz
 cd dist/deb/sudo-font
 mv sudo-font-${version}.tar.gz sudo-font_${version}.orig.tar.gz
 tar xzf sudo-font_${version}.orig.tar.gz
