fcc33108f5073c88aef01f547957dc032fe2b3ed
Jens Kutilek <webmail@kutilek.de>
2023-10-08 23:45:36 +0200

    Fix filename

--- scripts/prepare-deb.sh
+++ scripts/prepare-deb.sh
@@ -5,5 +5,5 @@ mkdir -p dist/deb/sudo-font
 git archive --format=tar.gz ${version} --prefix=sudo-font-${version}/ > dist/deb/sudo-font/sudo-font-${version}.tar.gz
 cd dist/deb/sudo-font
 mv sudo-font-${version}.tar.gz sudo-font_${version}-orig.tar.gz
-tar xzf sudo-font_${version}-orig.tar.gz
+tar xzf sudo-font_${version}.orig.tar.gz
 cp -R ../../../packaging/debian sudo-font-${version}
