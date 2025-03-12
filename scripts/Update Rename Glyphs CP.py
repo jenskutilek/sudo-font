93f7cd9f87e8b32ed328cefee1d128a244d6de3c
Jens Kutilek <webmail@kutilek.de>
2025-03-12 20:52:37 +0100

    Improve custom parameter script

--- scripts/Update Rename Glyphs CP.py
+++ scripts/Update Rename Glyphs CP.py
@@ -5,8 +5,11 @@ from GlyphsApp import Glyphs, GSCustomParameter
 all_names = [g.name for g in Glyphs.font.glyphs]
 ss20_names = [name for name in all_names if ".ss20" in name]
 ital_names = [name for name in all_names if ".italic" in name]
+zero_names = [name for name in all_names if ".zero" in name]
 ss20_pairs = []
 ital_pairs = []
+zero_pairs = []
+
 
 for ss20_name in ss20_names:
     default_name = ss20_name.replace(".ss20", "")
@@ -14,6 +17,7 @@ for ss20_name in ss20_names:
         print("Missing default glyph for SS20 glyph:", default_name)
         continue
     ss20_pairs.append((default_name, ss20_name))
+
 
 for ital_name in ital_names:
     default_name = ital_name.replace(".italic", "")
@@ -23,40 +27,50 @@ for ital_name in ital_names:
     ital_pairs.append((default_name, ital_name))
 
 
-def addSRenameParams(i, rename_ss20_pairs, rename_italic_pairs=[]):
+for zero_name in zero_names:
+    default_name = zero_name.replace(".zero", "")
+    if default_name not in all_names:
+        print("Missing default glyph for slashed zero glyph:", default_name)
+        continue
+    zero_pairs.append((default_name, zero_name))
+
+
+def addSRenameParams(
+    i, rename_ss20_pairs, rename_italic_pairs=[], rename_zero_pairs=[]
+):
     del_cp = []
     for n, cp in enumerate(i.customParameters):
         if cp.name == "Rename Glyphs":
             del_cp.append(n)
     for n in sorted(del_cp, reverse=True):
         del i.customParameters[n]
+    pairs = []
 
     if rename_ss20_pairs:
-        cp = GSCustomParameter(
-            "Rename Glyphs",
-            "\n".join([f"{pair[0]}={pair[1]}" for pair in rename_ss20_pairs]),
-        )
-        i.customParameters.append(cp)
-
+        pairs.extend(rename_ss20_pairs)
     if rename_italic_pairs:
+        pairs.extend(rename_italic_pairs)
+    if rename_zero_pairs:
+        pairs.extend(rename_zero_pairs)
+    if pairs:
         cp = GSCustomParameter(
             "Rename Glyphs",
-            "\n".join([f"{pair[0]}={pair[1]}" for pair in rename_italic_pairs]),
+            "\n".join([f"{pair[0]}={pair[1]}" for pair in pairs]),
         )
         i.customParameters.append(cp)
 
 
 uivar = Glyphs.font.instances[0]  # Sudo UI Var
-addSRenameParams(uivar, ss20_pairs)
+addSRenameParams(uivar, ss20_pairs, rename_zero_pairs=zero_pairs)
 
-for n in (10, 11, 12, 13, 14):
+for n in (7, 8, 9, 10, 11):
     si = Glyphs.font.instances[n]  # Mono Italics
     addSRenameParams(si, [], ital_pairs)
 
-for n in (15, 16, 17, 18, 19):
+for n in (12, 13, 14, 15, 16):
     si = Glyphs.font.instances[n]  # UI Romans
-    addSRenameParams(si, ss20_pairs)
+    addSRenameParams(si, ss20_pairs, rename_zero_pairs=zero_pairs)
 
-for n in (20, 21, 22, 23, 24):
+for n in (17, 18, 19, 20, 21):
     si = Glyphs.font.instances[n]  # UI Italics
-    addSRenameParams(si, ss20_pairs, ital_pairs)
+    addSRenameParams(si, ss20_pairs, ital_pairs, rename_zero_pairs=zero_pairs)
